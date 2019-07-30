0afeb1d6f77754a5b877dac990d3b4002bccefb4
Gary Odendaal <gary@Garys-MacBook-Pro.local>
2019-07-30 16:10:27 +0200

    Adding dDelete from state function

--- RenderingLists/src/App.js
+++ RenderingLists/src/App.js
@@ -5,24 +5,12 @@ import Person from "./Person/Person";
 class App extends Component {
 	state = {
 		persons: [
-			{ name: "Max", age: 28 },
-			{ name: "Manu", age: 29 },
-			{ name: "Stephanie", age: 26 }
+			{ id: "a1", name: "Max", age: 28 },
+			{ id: "a2", name: "Manu", age: 29 },
+			{ id: "a3", name: "Stephanie", age: 26 }
 		],
 		otherState: "some other value",
 		showPersons: false
-	};
-
-	switchNameHandler = newName => {
-		// console.log('Was clicked!');
-		// DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-		this.setState({
-			persons: [
-				{ name: newName, age: 28 },
-				{ name: "Manu", age: 29 },
-				{ name: "Stephanie", age: 27 }
-			]
-		});
 	};
 
 	nameChangedHandler = event => {
@@ -33,6 +21,13 @@ class App extends Component {
 				{ name: "Stephanie", age: 26 }
 			]
 		});
+	};
+
+	deletePersonHandler = personIndex => {
+		//const persons = this.state.persons.slice();
+		const persons = [...this.state.persons];
+		persons.splice(personIndex, 1);
+		this.setState({ persons: persons });
 	};
 
 	togglePersonsHandler = () => {
@@ -54,22 +49,16 @@ class App extends Component {
 		if (this.state.showPersons) {
 			persons = (
 				<div>
-					<Person
-						name={this.state.persons[0].name}
-						age={this.state.persons[0].age}
-					/>
-					<Person
-						name={this.state.persons[1].name}
-						age={this.state.persons[1].age}
-						click={this.switchNameHandler.bind(this, "Max!")}
-						changed={this.nameChangedHandler}
-					>
-						My Hobbies: Racing
-					</Person>
-					<Person
-						name={this.state.persons[2].name}
-						age={this.state.persons[2].age}
-					/>
+					{this.state.persons.map((person, index) => {
+						return (
+							<Person
+								click={() => this.deletePersonHandler(index)}
+								name={person.name}
+								age={person.age}
+								key={person.id}
+							/>
+						);
+					})}
 				</div>
 			);
 		}

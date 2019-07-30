601778879aa80d5607cfb392b18f067f134adc1d
Gary Odendaal <gary@Garys-MacBook-Pro.local>
2019-07-30 15:31:24 +0200

    Checking node_modules

--- assignment1/src/UserInput/UserInput.js
+++ assignment1/src/UserInput/UserInput.js
@@ -3,7 +3,6 @@ import React from "react";
 const userInput = props => {
 	return (
 		<div>
-			<label for="" />
 			<input type="text" onChange={props.changed} value={props.username} />
 		</div>
 	);

42dcfed793397445123430447ae103c436d760fa
Gary Odendaal <gary@Garys-MacBook-Pro.local>
2019-07-31 10:52:10 +0200

    Second Style section complete

--- Styling2/src/App.js
+++ Styling2/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
-import Radium, { StyleRoot } from "radium";
+
 class App extends Component {
 	state = {
 		persons: [
@@ -40,27 +40,17 @@ class App extends Component {
 	};
 
 	render() {
-		const style = {
-			backgroundColor: "green",
-			font: "inherit",
-			border: "1px solid blue",
-			padding: "8px",
-			cursor: "pointer",
-			":hover": {
-				backgroundColor: "lightgreen",
-				color: "black"
-			}
-		};
-
-		const classes = [];
+		const assignedClasses = [];
 		if (this.state.persons.length <= 2) {
-			classes.push("red");
+			assignedClasses.push(classes.red);
 		}
 		if (this.state.persons.length <= 1) {
-			classes.push("bold");
+			assignedClasses.push(classes.bold);
 		}
 
 		let persons = null;
+
+		let btnClass = "";
 
 		if (this.state.showPersons) {
 			persons = (
@@ -78,28 +68,21 @@ class App extends Component {
 					})}
 				</div>
 			);
-
-			style.backgroundColor = "orange";
-			style[":hover"] = {
-				backgroundColor: "yellow",
-				color: "black"
-			};
+			btnClass = classes.red;
 		}
 
 		return (
-			<StyleRoot>
-				<div className="App">
-					<h1>Hi, I'm a React App</h1>
-					<p className={classes.join(" ")}>This is really working!</p>
-					<button style={style} onClick={this.togglePersonsHandler}>
-						Toggle Persons
-					</button>
-					{persons}
-				</div>
-			</StyleRoot>
+			<div className={classes.App}>
+				<h1>Hi, I'm a React App</h1>
+				<p className={assignedClasses.join(" ")}>This is really working!</p>
+				<button className={btnClass} onClick={this.togglePersonsHandler}>
+					Toggle Persons
+				</button>
+				{persons}
+			</div>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 	}
 }
 
-export default Radium(App);
+export default App;

6710b6bd0755484a05bcf68ff584c0004c17a8af
Gary Odendaal <gary@Garys-MacBook-Pro.local>
2019-07-31 14:22:28 +0200

    Updating State Management

---
+++ ComponentDeepDive/src/components/Persons/Persons.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+import Person from "./Person/Person";
+
+class Persons extends Component {
+	// static getDerivedStateFromProps(props, state) {
+	// 	console.log("[Persons.js] getDerivedStateFromProps", props);
+	// 	return state;
+	// }
+
+	// componentWillReceiveProps(props) {
+	// 	console.log("[Persons.js] componentWillReceiveProps");
+	// }
+
+	// componentWillUpdate(prevProps, prevState) {
+	// 	console.log("[Persons.js] componentWillUpdate");
+	// }
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log("[Persons.js] shouldComponentUpdate", nextProps);
+		return true;
+	}
+
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		console.log("[Persons.js] getSnapshotBeforeUpdate", prevProps);
+		return { message: "Snapshot!" };
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		console.log("[Persons.js] componentDidUpdate");
+		console.log(snapshot);
+	}
+
+	render() {
+		console.log("[Persons.js] rendering...");
+		return this.props.persons.map((person, index) => {
+			return (
+				<Person
+					click={() => this.props.clicked(index)}
+					name={person.name}
+					age={person.age}
+					key={person.id}
+					changed={event => this.props.changed(event, person.id)}
+				/>
+			);
+		});
+	}
+}
+
+export default Persons;
---
+++ ComponentDeepDive/src/containers/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+
+import classes from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		console.log("[App.js] constructor");
+	}
+
+	static getDerivedStateFromProps(props, state) {
+		console.log("[App.js] getDerivedStateFromProps", props);
+		return state;
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log("[App.js] shouldComponentUpdate", nextProps);
+		return true;
+	}
+
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		console.log("[App.js] getSnapshotBeforeUpdate", prevProps);
+		return prevState;
+	}
+	// componentWillMount() {
+	// 	console.log("[App.js] componentWillMount");
+	// }
+
+	componentDidMount() {
+		console.log("[App.js] componentDidMount");
+	}
+
+	componentDidUpdate() {
+		console.log("[App.js] componentDidUpdate");
+	}
+
+	state = {
+		persons: [
+			{ id: "a1", name: "Max", age: 28 },
+			{ id: "a2", name: "Manu", age: 29 },
+			{ id: "a3", name: "Stephanie", age: 26 }
+		],
+		otherState: "some other value",
+		showPersons: false
+	};
+
+	nameChangedHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex(p => {
+			return p.id === id;
+		});
+
+		const person = {
+			...this.state.persons[personIndex]
+		};
+		person.name = event.target.value;
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
+		this.setState({ persons: persons });
+	};
+
+	deletePersonHandler = personIndex => {
+		//const persons = this.state.persons.slice();
+		const persons = [...this.state.persons];
+		persons.splice(personIndex, 1);
+		this.setState({ persons: persons });
+	};
+
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons;
+		this.setState({ showPersons: !doesShow });
+	};
+
+	render() {
+		console.log("[App.js] Render");
+		let persons = null;
+
+		if (this.state.showPersons) {
+			persons = (
+				<Persons
+					persons={this.state.persons}
+					clicked={this.deletePersonHandler}
+					changed={this.nameChangedHandler}
+				/>
+			);
+		}
+
+		return (
+			<div className={classes.App}>
+				<Cockpit
+					title={this.props.appTitle}
+					showPersons={this.state.showPersons}
+					persons={this.state.persons}
+					clicked={this.togglePersonsHandler}
+				/>
+				{persons}
+			</div>
+		);
+		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+	}
+}
+
+export default App;
